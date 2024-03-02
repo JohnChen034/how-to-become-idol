52f739385266ee8ea52b224a6267ef3575797983
JohnChen034 <81580002+JohnChen034@users.noreply.github.com>
2024-03-01 16:00:38 -0800

    update pyramid side stroke

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -14,14 +14,14 @@
 
   <svg width="1000" height="1000" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
     <!-- Draw the outer triangle -->
-    <polygon points="100,10 10,190 190,190" fill="none" stroke="black" stroke-width="0"/>
+<!--    <polygon points="100,10 10,190 190,190" fill="none" stroke="black" stroke-width="0"/>-->
 
     <!-- Draw and color each layer within the triangle with fade transition and hover effect -->
     {#if layerVisible > 0}
       <polygon in:fade={{ duration: 500 }}
                points="30,150 50,110 150,110 170,150"
                fill="#c6e5f3"
-               class="layer"
+               class="layer" stroke="black" stroke-width="0.5"
       />
     {/if}
     {#if layerVisible > 1}
@@ -29,20 +29,21 @@
                points="50,110 70,70 130,70 150,110"
                fill="#a4d4c4"
                class="layer"
+               stroke="black" stroke-width="0.5"
       />
     {/if}
     {#if layerVisible > 2}
       <polygon in:fade={{ duration: 500 }}
                points="70,70 85,40 115,40 130,70"
                fill="#f9e2af"
-               class="layer"
+               class="layer" stroke="black" stroke-width="0.5"
       />
     {/if}
     {#if layerVisible > 3}
       <polygon in:fade={{ duration: 500 }}
                points="100,10 85,40 115,40"
                fill="#e3c1f4"
-               class="layer"
+               class="layer" stroke="black" stroke-width="0.5"
       />
     {/if}
   </svg>
