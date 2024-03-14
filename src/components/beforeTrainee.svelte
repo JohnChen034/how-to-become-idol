24527839b19a9fb0651bc818768755b53f5c709c
Darren Liu <dayuanliu11@gmail.com>
2024-03-14 13:05:54 -0700

    a

--- src/components/beforeTrainee.svelte
+++ src/components/beforeTrainee.svelte
@@ -14,8 +14,17 @@
     <h2>2. Agency Audition</h2>
     <p>Many K-pop agencies conduct various auditions to recruit new talent.</p>
     <p>There can be periodic auditions that agencies announce, ongoing recruitment auditions, and national or global audition tours.</p>
-    <p>aaaaa</p>
     <p></p>
     <p></p>
     <p></p>
+    <p></p>
+
+    <h2>3. Connecting with Idol Academy</h2>
+    <p>You can also enter an agency through an academy that prepares you to be an idol. </p>
+    <p>They focus on dance and vocal training. Some agencies run their own academies.</p>
+    <p>cost about 400,000 won to 600,000 won a month (approx. 300 to 500 USD)</p>
+    <p>If the academy is closely connected to the agency, they recommend talented students to the agency.</p>
+    <p>Or, the agency can contact the academy after watching the student evaluation videos uploaded by the academy.</p>
+    <p>Also, in the case of large academies, agency representatives can visit the academy to hold auditions.</p>
+    <p>he competition rate is lower at these private auditions held in the academy, so chances of become a trainee are a little higher.</p>
 </div>
