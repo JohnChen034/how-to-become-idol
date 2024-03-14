4ef303e5c4f6762248070af5bc2eec25129af2d2
Darren Liu <dayuanliu11@gmail.com>
2024-03-14 13:02:20 -0700

    a

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -3,7 +3,6 @@
   import FlipCard from './FlipCard.svelte';
   import Timeline from './Timeline.svelte';
   import TraineeDebutRate from "./TraineeDebutRate.svelte";
-  import KpopTrendLineChart from "./kpopTrendLineChart.svelte"
   import BeforeTrainee from "./beforeTrainee.svelte";
   import { onMount } from "svelte";
 
@@ -30,8 +29,6 @@
   </div>
 
   <p>some content here, introduction stuff</p>
-
-  <KpopTrendLineChart {kpopTrendData}></KpopTrendLineChart>
 
   <BeforeTrainee/>
 
