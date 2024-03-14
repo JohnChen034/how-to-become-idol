dd33bfa29dc05c1773fbc80e8c09b71837414af5
Darren Liu <dayuanliu11@gmail.com>
2024-03-14 12:48:34 -0700

    a

--- src/components/beforeTrainee.svelte
+++ src/components/beforeTrainee.svelte
@@ -14,7 +14,7 @@
     <h2>2. Agency Audition</h2>
     <p>Many K-pop agencies conduct various auditions to recruit new talent.</p>
     <p>There can be periodic auditions that agencies announce, ongoing recruitment auditions, and national or global audition tours.</p>
-    <p></p>
+    <p>aa</p>
     <p></p>
     <p></p>
     <p></p>
