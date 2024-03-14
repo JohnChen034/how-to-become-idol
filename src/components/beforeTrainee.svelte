f6fbd54e9a545d6667820c1c466e0e346c14ea91
Darren Liu <dayuanliu11@gmail.com>
2024-03-14 12:38:03 -0700

    a

--- src/components/beforeTrainee.svelte
+++ src/components/beforeTrainee.svelte
@@ -1,22 +1,21 @@
-<main>
-<h1>How to become an Idol Trainee</h1>
-<p>In order to become an idol, you have to be an idol trainee who has a contract with an agency.</p>
-<p>Therefore, what are the paths to become an idol trainee?</p>
+<div class=container>
+    <h1>How to become an Idol Trainee</h1>
+    <p>In order to become an idol, you have to be an idol trainee who has a contract with an agency.</p>
+    <p>Therefore, what are the paths to become an idol trainee?</p>
 
-<h2>1. Street Casting<h2>
-<p>Street casting is literally being cast on the street.</p>
-<p>Each agency has a rookie development team that finds and fosters new talent.</p>
-<p>Casting staff in this rookie development team scouts and casts people on the streets.</p>
-<p>They approach someone to cast them if their appearance matches the image of the idol group they are planning.</p>
-<p>Just because you've been offered a position through street casting doesn't mean you can become an idol trainee right away.</p>
-<p>The contract is written after going through a very basic audition and camera test process.</p>
+    <h2>1. Street Casting<h2>
+    <p>Street casting is literally being cast on the street.</p>
+    <p>Each agency has a rookie development team that finds and fosters new talent.</p>
+    <p>Casting staff in this rookie development team scouts and casts people on the streets.</p>
+    <p>They approach someone to cast them if their appearance matches the image of the idol group they are planning.</p>
+    <p>Just because you've been offered a position through street casting doesn't mean you can become an idol trainee right away.</p>
+    <p>The contract is written after going through a very basic audition and camera test process.</p>
 
-<h2>2. Agency Audition<h2>
-<p>Many K-pop agencies conduct various auditions to recruit new talent.</p>
-<p>There can be periodic auditions that agencies announce, ongoing recruitment auditions, and national or global audition tours.</p>
-<p>Casting staff in this rookie development team scouts and casts people on the streets.</p>
-<p>They approach someone to cast them if their appearance matches the image of the idol group they are planning.</p>
-<p>Just because you've been offered a position through street casting doesn't mean you can become an idol trainee right away.</p>
-<p>The contract is written after going through a very basic audition and camera test process.</p>
-
-</main> 
+    <h2>2. Agency Audition<h2>
+    <p>Many K-pop agencies conduct various auditions to recruit new talent.</p>
+    <p>There can be periodic auditions that agencies announce, ongoing recruitment auditions, and national or global audition tours.</p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+</div>
