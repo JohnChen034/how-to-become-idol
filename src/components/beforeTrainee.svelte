b60ac9db865291d27fe8fe807923ac75226cf4a5
Darren Liu <dayuanliu11@gmail.com>
2024-03-14 12:47:15 -0700

    a

--- src/components/beforeTrainee.svelte
+++ src/components/beforeTrainee.svelte
@@ -3,7 +3,7 @@
     <p>In order to become an idol, you have to be an idol trainee who has a contract with an agency.</p>
     <p>Therefore, what are the paths to become an idol trainee?</p>
 
-    <h2>1. Street Casting<h2>
+    <h2>1. Street Casting</h2>
     <p>Street casting is literally being cast on the street.</p>
     <p>Each agency has a rookie development team that finds and fosters new talent.</p>
     <p>Casting staff in this rookie development team scouts and casts people on the streets.</p>
@@ -11,7 +11,7 @@
     <p>Just because you've been offered a position through street casting doesn't mean you can become an idol trainee right away.</p>
     <p>The contract is written after going through a very basic audition and camera test process.</p>
 
-    <h2>2. Agency Audition<h2>
+    <h2>2. Agency Audition</h2>
     <p>Many K-pop agencies conduct various auditions to recruit new talent.</p>
     <p>There can be periodic auditions that agencies announce, ongoing recruitment auditions, and national or global audition tours.</p>
     <p></p>
