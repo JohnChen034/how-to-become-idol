e5d1dadd5537ab08d19a1a78de4beb4a0c1df893
Darren Liu <dayuanliu11@gmail.com>
2024-03-15 13:57:21 -0700

    a

--- src/components/hook.svelte
+++ src/components/hook.svelte
@@ -21,13 +21,15 @@
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap');
   @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Archivo+Black&family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap')
 
   .sentence {
     opacity: 0;
     animation: fadeIn 1s forwards;
     margin-bottom: 10px;
-    font-size: 50px;
-    font-family: 'Archivo', sans-serif;
+    font-size: 30px;
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-weight: 100
     text-align: left;
     color: #423C3C; /* dark Grey */;
   }
