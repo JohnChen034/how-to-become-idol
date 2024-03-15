de593cdda178d0747bab82c4c3453bce81984505
Darren Liu <dayuanliu11@gmail.com>
2024-03-15 02:02:34 -0700

    a

--- src/components/hook.svelte
+++ src/components/hook.svelte
@@ -23,6 +23,8 @@
     opacity: 0;
     animation: fadeIn 1s forwards;
     margin-bottom: 10px; /* Space between sentences */
+    font-size: 50px
+    font-family: Arial, sans-serif
   }
 
   @keyframes fadeIn {
@@ -37,12 +39,3 @@
     <div class="sentence" style="animation-delay: {index * 0.5}s">{sentence}</div>
   {/each}
 </div>
-
-
-
-
-
-<p>Have you ever think of becoming an K-pop idol?</p>
-<p>If yes</p>
-<p>Do you know how difficult it is to be selected as a trainee in the audition</p>
-<p>Do you know how much effort it takes to debut</p>
