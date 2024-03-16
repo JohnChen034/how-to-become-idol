5424124f9d43492b6185e568d6e27938d07d1a28
Darren Liu <dayuanliu11@gmail.com>
2024-03-15 22:47:02 -0700

    Update loseWeight.svelte

--- src/components/loseWeight.svelte
+++ src/components/loseWeight.svelte
@@ -49,19 +49,31 @@
     To maintain their body, idols usually have to consume only 30%-60% of their bmi
 </p>
 
-<div class="suzy_container">
+<div style="display: flex; flex-direction: row;">
     <p style="font-family: 'IBM Plex Sans', sans-serif; font-weight: 600; font-size:35px; color:#393537;">
         The left images illustrate a day of food choices from Suzy Bae Sue Ji for 5 months. The total calories for her meals is only 1300!
     </p>
-    <img src="loseWeight/suzy.png" style="width: 200px; height: 500px; display: flex" alt="">
-    <FoodChoices imgLinks={breakfastImageLinks} mealName="breakfast"></FoodChoices>
-    <FoodChoices imgLinks={lunchImageLinks} mealName="lunch"></FoodChoices>
-    <FoodChoices imgLinks={dinnerImageLinks} mealName="dinner"></FoodChoices>
-</div>
-
-<div class="weight_calc_container">
     <p style="font-family: 'IBM Plex Sans', sans-serif; font-weight: 600; font-size:35px; color:#393537;">
         The right one shows how much food 1300 calories equal to
     </p>
+</div>
+
+
+<div style="display: flex;     flex-direction: row; width: 100%; height: 450px">
+
+    <div class="photo">
+        <div
+                id="img"
+                alt="此处放置图片" style="margin-left: 50px;">
+            <img src="loseWeight/suzy.png" style="width: 200px; height: 500px; display: flex" alt="">
+        </div>
+
+    </div>
+    <div style="display: flex;     flex-direction: column;">
+        <FoodChoices imgLinks={breakfastImageLinks} mealName="breakfast"></FoodChoices>
+        <FoodChoices imgLinks={lunchImageLinks} mealName="lunch"></FoodChoices>
+        <FoodChoices imgLinks={dinnerImageLinks} mealName="dinner"></FoodChoices>
+    </div>
     <FoodCaloriesIntake></FoodCaloriesIntake>
+
 </div>
