4740e6be8ec2d581fb68980c25599bee3b063ed5
Darren Liu <dayuanliu11@gmail.com>
2024-03-15 20:36:52 -0700

    Update BTStwo.svelte

--- src/components/BTStwo.svelte
+++ src/components/BTStwo.svelte
@@ -1,41 +1,39 @@
 <script>
-  import Carousel from "svelte-carousel";
-  const images = [
-    {
-      url: "https://res.cloudinary.com/beswift/image/upload/v1650391490/photo-1644241687200-eadaf7601290_xcz2kh.jpg",
-      description: "image1",
-    },
-    {
-      url: "https://res.cloudinary.com/beswift/image/upload/v1650391337/photo-1647067867267-e01d98462f3c_ugtnwe.jpg",
-      description: "image2",
-    },
-    {
-      url: "https://res.cloudinary.com/beswift/image/upload/v1650391131/photo-1648800475313-2bb7fbec8701_ae60yw.jpg",
-      description: "image3",
-    },
-    {
-      url: "https://res.cloudinary.com/beswift/image/upload/v1650390102/photo-1649894222226-056a1a79d9fb_xlv73h.jpg",
-      description: "image4",
-    },
-    {
-      url: "https://res.cloudinary.com/beswift/image/upload/v1650389425/photo-1649894221695-45abb718a190_sgjhwd.jpg",
-      description: "image5",
-    },
+  let images = [
+    'bts.png'
   ];
+  let currentImageIndex = 0;
+
+  function showNext() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+  }
+
+  function showPrevious() {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+  }
 </script>
 
-<Carousel
-    bind:this={carousel}
-    let:loaded
-    autoplay
-    autoplayDuration={3000}
-    autoplayProgressVisible
->
-    {#each images as src, imageIndex (src)}
-      <div class="img-container">
-        {#if loaded.includes(imageIndex)}
-          <img src={src.url} alt={src.description} width={1000} height={600} />
-        {/if}
-      </div>
-    {/each}
-</Carousel>
+<style>
+  .carousel-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  }
+
+  img {
+    max-width: 100%;
+    border-radius: 10px; /* Optional: for rounded corners */
+  }
+
+  button {
+    padding: 10px;
+    cursor: pointer;
+  }
+</style>
+
+<div class="carousel-container">
+  <button on:click={showPrevious}>Previous</button>
+  <img src={images[currentImageIndex]} alt="Carousel Image">
+  <button on:click={showNext}>Next</button>
+</div>
