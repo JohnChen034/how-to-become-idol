71f092953f86b4b3fc6f7e561b5434a2d1e0e802
Darren Liu <dayuanliu11@gmail.com>
2024-03-15 17:39:58 -0700

    Update ScrollyTeller.svelte

--- src/components/ScrollyTeller.svelte
+++ src/components/ScrollyTeller.svelte
@@ -48,7 +48,7 @@
     </section>
 
 
-    <section style="background-color:#b37eb5;">
+    <section style="background-color:#b37eb5; justify-content: flex-start; flex-direction: row;">
       <div class="container">
         <BTSone />
       </div>
@@ -126,6 +126,10 @@
     gap: 20px;
     padding: 20px;
     background-color: #a27eac;
+    display: flex; /* Use flexbox for positioning the child */
+    justify-content: center; /* Center horizontally */
+    align-items: center; /* Center vertically */
+    flex-direction: column; /* Stack children vertically */
     border-radius: 50px 50px 50px 50px;
     overflow: hidden;
   }
