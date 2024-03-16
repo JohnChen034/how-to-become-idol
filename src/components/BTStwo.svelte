8125b8f0bddf9428f80344fd8ab0080adef4b18c
Darren Liu <dayuanliu11@gmail.com>
2024-03-15 20:32:12 -0700

    Update BTStwo.svelte

--- src/components/BTStwo.svelte
+++ src/components/BTStwo.svelte
@@ -2,30 +2,26 @@
   import Carousel from "svelte-carousel";
   const images = [
     {
-      url: "/static/difficulty1.PNG",
+      url: "https://res.cloudinary.com/beswift/image/upload/v1650391490/photo-1644241687200-eadaf7601290_xcz2kh.jpg",
       description: "image1",
     },
     {
-      url: "/static/difficulty2.PNG",
+      url: "https://res.cloudinary.com/beswift/image/upload/v1650391337/photo-1647067867267-e01d98462f3c_ugtnwe.jpg",
       description: "image2",
     },
     {
-      url: "/static/difficulty3.PNG",
+      url: "https://res.cloudinary.com/beswift/image/upload/v1650391131/photo-1648800475313-2bb7fbec8701_ae60yw.jpg",
       description: "image3",
     },
     {
-      url: "/static/difficulty4.PNG",
+      url: "https://res.cloudinary.com/beswift/image/upload/v1650390102/photo-1649894222226-056a1a79d9fb_xlv73h.jpg",
       description: "image4",
     },
     {
-      url: "/static/difficulty5.PNG",
+      url: "https://res.cloudinary.com/beswift/image/upload/v1650389425/photo-1649894221695-45abb718a190_sgjhwd.jpg",
       description: "image5",
     },
   ];
-  let carousel; // for calling methods of the carousel instance
-  const handleNextClick = () => {
-    carousel.goToNext();
-  };
 </script>
 
 <Carousel
